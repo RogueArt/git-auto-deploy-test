9a3c214cf8536acb2d2574231ac948f80ed89e22
Rogue Art <rogueart.dev@outlook.com>
2021-07-13 22:02:38 +0000

    Set up an Express server with some dynamic templating

---
+++ main.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+console.log(__dirname)
+app.use(express.static(__dirname))
+app.set('view engine', 'ejs')
+
+const people = ['a', 'b', 'c']
+let count = 0
+
+// Index page
+app.get('/', (req, res) => {
+  count += 1
+  res.render('index.ejs', { people, count })
+})
+
+// Start the server
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at http://localhost:${process.env.PORT}`)
+})
